242ecdef3bde5133ab399cf1663691c55a7d4b4f
jmacey <jmacey@bournemouth.ac.uk>
2021-11-01 11:19:07 +0000

    updated for Qt6

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -483,11 +483,11 @@ void NGLScene::wheelEvent( QWheelEvent* _event )
 {
 
   // check the diff of the wheel position (0 means no change)
-  if ( _event->delta() > 0 )
+  if ( _event->angleDelta().x() > 0 )
   {
     m_modelPos.m_z += ZOOM;
   }
-  else if ( _event->delta() < 0 )
+  else if ( _event->angleDelta().x() < 0 )
   {
     m_modelPos.m_z -= ZOOM;
   }
